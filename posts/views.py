f2db3cac7051e63970b2c3169e5c27a67f51505a
acrius <acrius@mail.ru>
2017-10-19 22:32:09 +0700

    Work on comment filters

--- posts/views.py
+++ posts/views.py
@@ -3,7 +3,7 @@ from rest_framework.viewsets import ModelViewSet
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer, UserSerializer
 from .permissions import IsOwnerOrReadOnly, ReadOnly
-
+import logging
 
 class PostViewSet(ModelViewSet):
     queryset = Post.objects.all().order_by('-publication_date')
@@ -18,6 +18,10 @@ class CommentViewSet(ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+    def get_queryset(self):
+        post = self.kwargs.get('post', 0)
+        return Comment.objects.all().order_by('-publication_date').filter(post__id=post)
 
 
 class UserViewSet(ModelViewSet):
